326e7ae9149d2984238b4e2e74ffce7e14b078a6
Sourav Agarwal <sourav.agarwal.con@hotstar.com>
2025-04-06 13:13:26 +0530

    Add database and gorm setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"net/http"
+	"os"
 )
 
 func main() {
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Error connecting to database")
+		return
+	}
+	fmt.Println("Connected to database", db)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
